2ca4d8a303cc8c91741c46ec2f549257192ef862
MerkiiChess <117251267+MerkiiChess@users.noreply.github.com>
2022-11-09 19:41:17 +0300

    Update DamageListener.java

--- src/ru/merkii/damagemat/listeners/DamageListener.java
+++ src/ru/merkii/damagemat/listeners/DamageListener.java
@@ -7,22 +7,14 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import ru.merkii.damagemat.DamageMain;
 
 public class DamageListener implements Listener {
-	private DamageMain plugin;
-	public DamageListener(final DamageMain plugin) {
-		this.plugin = plugin;
-	}
 	@EventHandler
 	public void onChat(final AsyncPlayerChatEvent e) {
 		final Player p = e.getPlayer();
-		if(this.plugin.getConfig().getStringList("Words").contains(e.getMessage())) {
-			p.damage(this.plugin.getConfig().getInt("Damage"));
-			if(this.plugin.getConfig().getBoolean("DelMessage")) {
-				e.setCancelled(true);
-			} else {
-				e.setCancelled(false);
-			}
+		if(DamageMain.getInstance().getConfig().getStringList("Words").contains(e.getMessage())) {
+			p.damage(DamageMain.getInstance().getConfig().getInt("Damage"));
+			e.setCancelled(DamageMain.getInstance().getConfig().getBoolean("DelMessage"));
 			if(p.getHealthScale() <= 0) {
-				e.setMessage(this.plugin.getConfig().getString("DeathMessage").replaceAll("%player%", p.getName()).replaceAll("&", "§"));
+				e.setMessage(DamageMain.getInstance().getConfig().getString("DeathMessage"));
 			}
 		}
 	}
