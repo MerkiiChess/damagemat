afd5fa42879230563472afc3c39a7acd1601a965
MerkiiChess <117251267+MerkiiChess@users.noreply.github.com>
2022-11-09 19:40:35 +0300

    Update DamageMain.java

--- src/ru/merkii/damagemat/DamageMain.java
+++ src/ru/merkii/damagemat/DamageMain.java
@@ -5,8 +5,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 import ru.merkii.damagemat.listeners.DamageListener;
 
 public class DamageMain extends JavaPlugin {
+	private static DamageMain instance;
 	public void onEnable() {
-		Bukkit.getPluginManager().registerEvents(new DamageListener(this), this);
+		Bukkit.getPluginManager().registerEvents(new DamageListener(), this);
 		saveDefaultConfig();
+		instance = this;
+	}
+	public static DamageMain getInstance() {
+		return DamageMain.instance;
 	}
 }
